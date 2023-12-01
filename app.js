89d41878eb7c6810a58d18b9f769452fb4ccda08
Asroyx <mystoganza@hotmail.com>
2023-12-01 23:52:30 +0300

    Initialize API URL and DOM elements

---
+++ app.js
@@ -0,0 +1,4 @@
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result = document.querySelector(".result");
+const sound = document.querySelector("#sound");
+const btn = document.querySelector("#search-btn");
