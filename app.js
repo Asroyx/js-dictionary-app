4c93d1e373d719eb371f6fb9090bfc2cd25ed2cd
Asroyx <mystoganza@hotmail.com>
2023-12-02 23:32:52 +0300

    Fix search-btn id name

--- app.js
+++ app.js
@@ -2,3 +2,12 @@ const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 const result = document.querySelector(".result");
 const sound = document.querySelector("#sound");
 const btn = document.querySelector("#search-btn");
+
+btn.addEventListener("click", () => {
+    let inpWord = document.querySelector("#inp-word").value;
+  
+    fetch(`${url}${inpWord}`)
+      .then(response => response.json())
+      .then(data => console.log(data));
+  });
+  
